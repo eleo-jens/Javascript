13f071e34f9989ff64924c3eddddf2f07761b44d
Eleonore <eleonore.jens@gmail.com>
2022-12-01 12:33:40 +0100

    alert

--- medical_rdv/scripts/main.js
+++ medical_rdv/scripts/main.js
@@ -1,5 +1,7 @@
 window.onload = function () {
 
+    let form = document.querySelector('form');
+
     // gestion de l'envoie du formulaire - désactivation du submit
     document.querySelector('button').addEventListener("click", (e) => {
         e.preventDefault;
@@ -8,7 +10,7 @@ window.onload = function () {
 
     // fonction qui récupère et valide les données du formulaire
     const get_data = function () {
-        patient = {
+        patient_brut = {
             'civilite': document.querySelector('select').value,
             'nom': document.querySelector('input[name="nom"]').value,
             'prenom': document.querySelector('input[name="prenom"]').value,
@@ -17,9 +19,29 @@ window.onload = function () {
             'date': '',
             'heure': ''
         };
-        console.log(patient)
+        console.log(patient_brut);
         
+        name_control(patient_brut.nom, document.querySelector('input[name="nom"]'));
+        // name_control(patient_brut.prenom, document.querySelector('input[name="prenom"]'));
     }
 
+    const name_control = function (name, input) {
+        let regExpression = /^[a-zA-Z]+ [a-zA-Z]+$/;
+        if (name.length < 2 || (!regExpression.test(name))) {
+            console.log('Entrez un nom valide!');
+            console.log(input);
+            input.focus();
+            let p = document.createElement('p');
+            p.innerText = "Veuillez insérer des données valides.";
+            p.classList.add('alert');
+            form.appendChild(p);
+            let i = document.createElement('i');
+            i.classList.add("fa-solid");
+            i.classList.add("fa-triangle-exclamation");
+            input.parentNode.append(i);
+            // <i class="fa-solid fa-triangle-exclamation"></i>
+
+        }
+    };
 
 };
